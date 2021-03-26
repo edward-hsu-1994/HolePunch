2ce8b52953af2afcd22e66ca77c67ac201ceccad
Edward Hsu <st89382000@gmail.com>
2021-03-26 23:42:37 +0800

    feat: test tcp proxy

---
+++ src/HolePunch.Accesses.Proxies.TestConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using Firewall;
+
+using System;
+using System.Net;
+
+namespace HolePunch.Accesses.Proxies.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proxyServer = new FirewallTcpProxyServer(IPEndPoint.Parse("192.168.31.1:80"));
+            proxyServer.OnConnected += (session) =>
+            {
+                Console.WriteLine("New Connection");
+            };
+            proxyServer.Start().Wait();
+
+            Console.WriteLine($"Port: {proxyServer.ListenPort}");
+
+            proxyServer.UpdateAllowCidrList(new CIDRNotation[] { CIDRNotation.Parse("0.0.0.0/0") });
+
+            Console.Read();
+        }
+    }
+}
