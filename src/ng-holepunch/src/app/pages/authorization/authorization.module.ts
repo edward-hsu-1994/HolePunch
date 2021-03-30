a7bea103253030711592be19aca6ca1d4698d06c
Edward Hsu <st89382000@gmail.com>
2021-03-31 00:22:51 +0800

    feat: implement login panel

--- src/ng-holepunch/src/app/pages/authorization/authorization.module.ts
+++ src/ng-holepunch/src/app/pages/authorization/authorization.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthorizationRoutingModule } from './authorization-routing.module';
-
+import { LoginComponent } from './components/login/login.component';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent],
   imports: [
     CommonModule,
-    AuthorizationRoutingModule
+    AuthorizationRoutingModule,
+    FormsModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzInputModule,
+    NzIconModule,
+    NzButtonModule
   ]
 })
 export class AuthorizationModule { }
