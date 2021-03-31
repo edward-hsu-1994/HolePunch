e9d3793285cc7294baf67d5734f82f8af6148cd4
Edward Hsu <st89382000@gmail.com>
2021-03-31 21:49:19 +0800

    feat: add dashbaord route guard

--- src/ng-holepunch/src/app/pages/dashboard/dashboard-routing.module.ts
+++ src/ng-holepunch/src/app/pages/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainframeComponent } from './components/mainframe/mainframe.component';
+import { IsAdminGuard } from './guards/is-admin.guard';
 
 const routes: Routes = [
-  { path:'', component: MainframeComponent}
+  {
+    path:'',
+    component: MainframeComponent,
+    canActivate:[IsAdminGuard]
+  }
 ];
 
 @NgModule({
