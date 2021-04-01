be9905a47853aff490431d3ed66283123fbe52af
Edward Hsu <st89382000@gmail.com>
2021-04-02 00:13:29 +0800

    feat: add service edit ui but not implement

--- src/ng-holepunch/src/app/pages/dashboard/pages/service-manage/components/edit-service/edit-service.component.ts
+++ src/ng-holepunch/src/app/pages/dashboard/pages/service-manage/components/edit-service/edit-service.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-service',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-service.component.scss']
 })
 export class EditServiceComponent implements OnInit {
-
-  constructor() { }
+  validateForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      id: [null],
+      name: [null, [Validators.required]],
+      port: [null, [Validators.required]],
+      logo_url: [null],
+      enabled: [false]
+    });
   }
 
+
+  submitForm(): void {
+    console.log(this.validateForm.value)
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
+  }
 }
