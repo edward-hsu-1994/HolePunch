9c4082bb6133f4ef67389393f69107c16773cea7
Edward Hsu <st89382000@gmail.com>
2021-04-02 22:54:21 +0800

    feat: log signalR error

--- src/ng-holepunch/src/app/pages/connect/components/connect/connect.component.ts
+++ src/ng-holepunch/src/app/pages/connect/components/connect/connect.component.ts
@@ -78,6 +78,7 @@ export class ConnectComponent implements OnInit {
         this.statusTitle = "Unsuccessfully Connected To Server!";
         this.statusDesc = "Please relogin or check your network.";
         this.icon = "close-circle";
+        console.log(err);
       });
   }
 
